d578827c979df9ce859e47d2d0a27b40935462ec
Thiago Felipe <thiagof.g3@gmail.com>
2021-10-09 15:38:50 -0300

    feat: add http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -20,3 +20,8 @@ export const ok = (data: any): HttpResponse => ({
     statusCode: 200,
     body: data
 })
+
+export const notFound = (error: Error): HttpResponse => ({
+    statusCode: 404,
+    body: error
+})
