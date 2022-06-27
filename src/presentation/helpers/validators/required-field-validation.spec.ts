a18fdf4b36c8596500fd2798416f92b1d4d84b37
Thiago Felipe <thiagof.g3@gmail.com>
2022-06-27 20:20:33 -0300

    test: ensure RequiredFieldValidation return null if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -8,4 +8,11 @@ describe('RequiredField Validation', () => {
 
         expect(response).toEqual(new MissingParamError('any_field'))
     })
+
+    test('Should return null if validation succeeds', () => {
+        const sut = new RequiredFieldValidation('any_field')
+        const response = sut.validate({ any_field: 'any_name' })
+
+        expect(response).toBeNull()
+    })
 })
