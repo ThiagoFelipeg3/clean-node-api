198bb71db4210943c8f5fd69bae3a2592416e54b
Thiago Felipe <thiagof.g3@gmail.com>
2022-08-01 11:48:04 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/accout-repository/account.spec.ts
+++ src/infra/db/mongodb/accout-repository/account.spec.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
@@ -18,7 +18,7 @@ describe('Account Mongo Repository', () => {
     })
 
     beforeEach(async () => {
-        accountCollection = await MongoHelper.getCollection('account')
+        accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.deleteMany({})
     })
 
@@ -60,5 +60,31 @@ describe('Account Mongo Repository', () => {
         const account = await sut.loadByEmail('anny_email@mail.com')
 
         expect(account).toBeNull()
+    })
+
+    test('Should update the account accessToken on updateAccessToen success', async () => {
+        const sut = makeSut()
+        const { insertedId: id } = await accountCollection.insertOne({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        })
+        const account = await accountCollection.findOne({ _id: id })
+        if (!account) {
+            return
+        }
+
+        expect(account.accessToken).toBeFalsy()
+
+        await sut.updateAccessToken(account._id.toHexString(), 'any_token')
+
+        const accountFind = await accountCollection.findOne({ _id: account._id })
+        if (!accountFind) {
+            return
+        }
+
+        expect(accountFind).toBeTruthy()
+        expect(accountFind.accessToken).toBe('any_token')
+        expect(accountFind.name).toBe('any_name')
     })
 })
