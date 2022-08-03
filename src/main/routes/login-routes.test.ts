943a70f9c6d3243359781599f67050b2c5379761
Thiago Felipe <thiagof.g3@gmail.com>
2022-08-03 09:01:20 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,6 +2,10 @@ import request from 'supertest'
 import app from '../config/app'
 
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
     beforeAll(async () => {
@@ -13,12 +17,12 @@ describe('Login Routes', () => {
     })
 
     beforeEach(async () => {
-        const accountCollection = await MongoHelper.getCollection('accounts')
+        accountCollection = await MongoHelper.getCollection('accounts')
         await accountCollection.deleteMany({})
     })
 
     describe('POST /signup', () => {
-        test('Should return 200 on signup', async () => {
+        test('Should return 201 on signup', async () => {
             await request(app)
                 .post('/api/signup')
                 .send({
@@ -28,6 +32,25 @@ describe('Login Routes', () => {
                     passwordConfirmation: '123'
                 })
                 .expect(201)
+        })
+    })
+
+    describe('POST /login', () => {
+        test('Should return 200 on login', async () => {
+            const password = await hash('123', 12)
+            await accountCollection.insertOne({
+                name: 'Thiago Felipe',
+                email: 'thiagof.g3@gmail.com',
+                password
+            })
+
+            await request(app)
+                .post('/api/login')
+                .send({
+                    email: 'thiagof.g3@gmail.com',
+                    password: '123'
+                })
+                .expect(200)
         })
     })
 })
